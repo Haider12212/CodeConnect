64c2c3979c5f18bf2984475ea77c8a52482d2070
Hasaan <hasaanahmad10023@gmail.com>
2024-05-18 15:17:23 +0500

    User Connection Established

---
+++ client/scripts/script.js
@@ -0,0 +1,7 @@
+const socket = io("http://localhost:3000/");
+
+socket.on('connection', () => {
+    console.log('connected');
+    }
+);
+
--- server/app.js
+++ server/app.js
@@ -1,35 +1,34 @@
 import express from 'express';
-import http from 'http';
-import { fileURLToPath } from 'url';  // Use 'url' instead of 'node:url'
-import { dirname, join } from 'path'; // Use 'path' instead of 'node:path'
+import { createServer } from 'http';
 import { Server } from 'socket.io';
 
+const PORT = 3000 || process.env.PORT;
+
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+
+});
 
 function generateUniqueCode() {
   return Math.random().toString(36).substring(2, 8).toUpperCase();
 }
 
-// Convert import.meta.url to a file path
-// path is static folder
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-
-
 app.get('/', (req, res) => {
-  res.sendFile(join(__dirname, './static/index.html'));
+  res.send('Hello World');
 });
 
+
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log('a user connected', socket.id);
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
 });
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log(`server running at ${PORT}`);
 });
